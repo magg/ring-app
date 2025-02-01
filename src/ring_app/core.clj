d8fe1e9e5562a81285cd8cee7a8b34afaa98fc23
Miguel Gonzalez <maggonzz@gmail.com>
2025-02-01 14:03:08 +0100

    added usage of Reitit

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,46 +1,55 @@
-;---
-; Excerpted from "Web Development with Clojure, Third Edition",
-; published by The Pragmatic Bookshelf.
-; Copyrights apply to this code. It may not be used to create training material,
-; courses, books, articles, and the like. Contact us if you are in doubt.
-; We make no guarantees that this code is fit for any purpose.
-; Visit http://www.pragmaticprogrammer.com/titles/dswdcloj3 for more book information.
-;---
-;
-;
 (ns ring-app.core
-  (:require
-    ;
-    ;;...
-    ;
-    ;
-    [ring.adapter.jetty :as jetty]
-    [ring.util.http-response :as response]
-    ;
-    [ring.middleware.reload :refer [wrap-reload]]))
-;
-
-(defn handler [request-map]
-  (response/ok
-    (str "<html><body> your IP is: "
-         (:remote-addr request-map)
-         "</body></html>")))
-;
+    (:require
+      [reitit.ring :as reitit]
+      [muuntaja.middleware :as muuntaja]
+      [ring.adapter.jetty :as jetty]
+      [ring.util.http-response :as response]
+      [ring.middleware.reload :refer [wrap-reload]]))
+(defn html-handler [request-map]
+      (response/ok
+        (str "<html><body> your IP is: "
+             (:remote-addr request-map)
+             "</body></html>")))
 
 (defn wrap-nocache [handler]
-  (fn [request]
-    (-> request
-        handler
-        (assoc-in [:headers "Pragma"] "no-cache"))))
+      (fn [request]
+          (-> request
+              handler
+              (assoc-in [:headers "Pragma"] "no-cache"))))
+(defn wrap-formats [handler]
+      (-> handler
+          (muuntaja/wrap-format)))
 
-;
+(def routes
+  [["/" html-handler]
+   ["/echo/:id"
+    {:get
+     (fn [{{:keys [id]} :path-params}]
+       (response/ok (str "<p>the value is: " id "</p>")))}]
+   ["/api" {:middleware [wrap-formats]}
+    ["/multiply"
+     {:post
+      (fn [{{:keys [a b]} :body-params}]
+        (response/ok {:result (* a b)}))}]]])
+
+(def handler
+  (reitit/ring-handler
+    (reitit/router routes)
+    (reitit/routes
+      (reitit/create-resource-handler {:path "/"})
+      (reitit/create-default-handler
+        {:not-found
+         (constantly (response/not-found "404 - Page not found"))
+         :method-not-allowed
+         (constantly (response/method-not-allowed "405 - Not allowed"))
+         :not-acceptable
+         (constantly (response/not-acceptable "406 - Not acceptable"))}))))
+
 
 (defn -main []
   (jetty/run-jetty
-    (-> handler
-        var
+    (-> #'handler
         wrap-nocache
         wrap-reload)
     {:port 3000
      :join? false}))
-;
